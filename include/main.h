2bf52a1e68399e203175fbc0761d85b8de1a4b82
MidoOK486 <mohamedzeidan2012@gmail.com>
2025-05-30 19:32:58 +0200

    main.h

---
+++ include/main.h
@@ -0,0 +1,12 @@
+// CRSP/include/main.h
+#pragma once
+
+namespace crsp {
+
+// Optional: system initialization before running
+void initialize_crsp();
+
+// Optional: cleanup after run
+void shutdown_crsp();
+
+}
