32c7b0289a740b60d1ba8cd46dca1f15e2890d1b
MidoOK486 <mohamedzeidan2012@gmail.com>
2025-05-30 18:49:02 +0200

    Update execute.cpp

--- src/instructions/execute.cpp
+++ src/instructions/execute.cpp
@@ -1,8 +1,10 @@
 // CRSP/src/instructions/execute.cpp
+
 #include "instructions/execute.h"
-#include "instructions/scalar/scalar_ops.h"
+#include "instructions/scalar/ADDI.h"
 #include "instructions/vector/vector_ops.h"
 #include "instructions/system/system_ops.h"
+#include <stdexcept>
 
 namespace crsp {
 
@@ -14,6 +16,9 @@ void execute_instruction(const DecodedInstruction& instr) {
             break;
 
         case 0x08: // ADDI
+            execute_scalar(instr);
+            break;
+
         case 0x09: // ADDIU
         case 0x0C: // ANDI
         case 0x0D: // ORI
@@ -21,23 +26,61 @@ void execute_instruction(const DecodedInstruction& instr) {
             execute_scalar(instr);
             break;
 
-        case 0x20: // LB/LW
-        case 0x28: // SB/SW
+        case 0x20: // LB / LW
+        case 0x28: // SB / SW
             execute_scalar(instr);
             break;
 
-        case 0x12: // COP2 (Vector unit)
+        case 0x12: // COP2 (Vector Unit)
             execute_vector(instr);
             break;
 
-        case 0x3F: // HALT or reserved
+        case 0x3F: // HALT or Reserved
             execute_system(instr);
             break;
 
         default:
-            // Unsupported opcode
             throw std::runtime_error("Unhandled opcode in execute_instruction");
     }
+}
+
+void execute_scalar(const DecodedInstruction& instr) {
+    switch (instr.op) {
+        case 0x08: // ADDI
+            execute_ADDI(instr);
+            break;
+
+        // Placeholders for future scalar instructions
+        case 0x09: // ADDIU
+        case 0x0C: // ANDI
+        case 0x0D: // ORI
+        case 0x0E: // XORI
+        case 0x20: // LB
+        case 0x23: // LW
+        case 0x28: // SB
+        case 0x2B: // SW
+            // TODO: Implement these
+            throw std::runtime_error("Scalar opcode not yet implemented");
+            break;
+
+        default:
+            throw std::runtime_error("Unhandled scalar opcode in execute_scalar");
+    }
+}
+
+void execute_vector(const DecodedInstruction& instr) {
+    // TODO: Implement full vector dispatch
+    throw std::runtime_error("Vector instructions not yet implemented");
+}
+
+void execute_special(const DecodedInstruction& instr) {
+    // TODO: Implement R-type SPECIAL instructions (SLL, SRL, etc.)
+    throw std::runtime_error("SPECIAL instructions not yet implemented");
+}
+
+void execute_system(const DecodedInstruction& instr) {
+    // TODO: Implement system-level ops (HALT, etc.)
+    throw std::runtime_error("System instructions not yet implemented");
 }
 
 }
