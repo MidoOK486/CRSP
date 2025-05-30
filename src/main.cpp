b1c658c524a1780e9a79591e4962a5ea5f15d841
MidoOK486 <mohamedzeidan2012@gmail.com>
2025-05-30 19:32:23 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 // CRSP/src/main.cpp
 
 #include "core/rsp_core.h"
+#include "main.h"
 #include <iostream>
 
 int main() {
-    std::cout << "[CRSP] Starting RSP Execution...\n";
+    std::cout << "[CRSP] Initializing...\n";
+    crsp::initialize_crsp();
 
-    // Call the main RSP run loop
+    std::cout << "[CRSP] Starting RSP Execution...\n";
     crsp::run_cycle();
 
-    std::cout << "[CRSP] RSP Execution Complete.\n";
+    std::cout << "[CRSP] Shutting down...\n";
+    crsp::shutdown_crsp();
+
     return 0;
 }
