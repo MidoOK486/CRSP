670ef29bb170ac2a222bc5073fef84d54c2907f8
MidoOK486 <mohamedzeidan2012@gmail.com>
2025-05-31 12:04:57 +0200

    Update execute.cpp

--- src/instructions/execute.cpp
+++ src/instructions/execute.cpp
@@ -2,6 +2,7 @@
 
 #include "instructions/execute.h"
 #include "instructions/scalar/ADDI.h"
+#include "instructions/scalar/ADDIU.h"
 #include "instructions/vector/vector_ops.h"
 #include "instructions/system/system_ops.h"
 #include <stdexcept>
@@ -11,27 +12,24 @@ namespace crsp {
 void execute_instruction(const DecodedInstruction& instr) {
     switch (instr.op) {
         case 0x00:
-            // SPECIAL opcode group — check func
             execute_special(instr);
             break;
 
         case 0x08: // ADDI
-            execute_scalar(instr);
+            execute_ADDI(instr);
             break;
 
         case 0x09: // ADDIU
+            execute_ADDIU(instr);
+            break;
+
         case 0x0C: // ANDI
         case 0x0D: // ORI
         case 0x0E: // XORI
             execute_scalar(instr);
             break;
 
-        case 0x20: // LB / LW
-        case 0x28: // SB / SW
-            execute_scalar(instr);
-            break;
-
-        case 0x12: // COP2 (Vector Unit)
+        case 0x12: // COP2 Vector Unit
             execute_vector(instr);
             break;
 
@@ -46,20 +44,10 @@ void execute_instruction(const DecodedInstruction& instr) {
 
 void execute_scalar(const DecodedInstruction& instr) {
     switch (instr.op) {
-        case 0x08: // ADDI
-            execute_ADDI(instr);
-            break;
-
-        // Placeholders for future scalar instructions
-        case 0x09: // ADDIU
         case 0x0C: // ANDI
         case 0x0D: // ORI
         case 0x0E: // XORI
-        case 0x20: // LB
-        case 0x23: // LW
-        case 0x28: // SB
-        case 0x2B: // SW
-            // TODO: Implement these
+            // TODO: Implement these scalar instructions
             throw std::runtime_error("Scalar opcode not yet implemented");
             break;
 
@@ -69,17 +57,14 @@ void execute_scalar(const DecodedInstruction& instr) {
 }
 
 void execute_vector(const DecodedInstruction& instr) {
-    // TODO: Implement full vector dispatch
     throw std::runtime_error("Vector instructions not yet implemented");
 }
 
 void execute_special(const DecodedInstruction& instr) {
-    // TODO: Implement R-type SPECIAL instructions (SLL, SRL, etc.)
     throw std::runtime_error("SPECIAL instructions not yet implemented");
 }
 
 void execute_system(const DecodedInstruction& instr) {
-    // TODO: Implement system-level ops (HALT, etc.)
     throw std::runtime_error("System instructions not yet implemented");
 }
 
