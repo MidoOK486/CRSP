5dd68470b86893c2a896c4d3d6e1b70abc4a5bee
MidoOK486 <mohamedzeidan2012@gmail.com>
2025-05-31 12:00:26 +0200

    ADDIU.h

---
+++ include/instructions/scalar/ADDIU.h
@@ -0,0 +1,11 @@
+// CRSP/include/instructions/scalar/ADDIU.h
+#pragma once
+
+#include "decode/decoder.h"
+
+namespace crsp {
+
+// Executes the ADDIU instruction
+void execute_ADDIU(const DecodedInstruction& instr);
+
+}
