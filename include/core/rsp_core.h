e1a01f3d91e2f5e260232baeb4cc7559a912a658
MidoOK486 <mohamedzeidan2012@gmail.com>
2025-06-01 20:15:26 +0200

    Update rsp_core.h

--- include/core/rsp_core.h
+++ include/core/rsp_core.h
@@ -1,18 +1,8 @@
 #pragma once
 
-#ifdef _WIN32
-  #ifdef CRSP_EXPORTS
-    #define CRSP_API __declspec(dllexport)
-  #else
-    #define CRSP_API __declspec(dllimport)
-  #endif
-#else
-  #define CRSP_API __attribute__((visibility("default")))
-#endif
-
 namespace crsp {
 
-// Exported public API
-CRSP_API void run_cycle();
+// Execute one RSP cycle (fetch -> decode -> execute)
+void run_cycle();
 
 }
